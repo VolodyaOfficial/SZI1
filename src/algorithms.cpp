903ee7bb0dcc3ede9c54caf7e537db5f84be3343
Denys Syrykh <densyrykh@gmail.com>
2026-04-25 19:00:29 +0200

    AStar (manhattan)

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -2,6 +2,13 @@
 #include <string>
 #include "algorithms.h"
 
+#include <queue>
+#include <set>
+#include <map>
+#include <chrono>
+#include <algorithm>
+#include <cmath>
+
 using namespace std;
 
 static set<vector<int>> pathVisitedDFS;
@@ -17,88 +24,130 @@ vector<int> makeGoalState(int rows, int cols) {
   return goal;
 }
 
-int findZero(const vector<int>& startBoard, Node node){
-  for (int i = 0; i < Board.size(); i++) {
-    if (Board[i] == 0) {
-      node.zeroPos = i;
+int findZero(const vector<int>& startBoard){
+  for (int i = 0; i < startBoard.size(); ++i) {
+    if (startBoard[i] == 0) {
+      return i;
     }
   }
-  return node.zeroPos;
+  return -1;
  }
 
-static bool DFSrec(const vector<int>& Board, int rows, int cols,
-              const string& path, int zeroPos=0, int depth=0,
+int manhattan(const vector<int>& board, int cols) {
+  int dist = 0;
+
+  for (int i = 0; i < board.size(); ++i) {
+    if (board[i] != 0) {
+      int value = board[i];
+      int row = i / cols;
+      int col = i % cols;
+
+      int goalRow = (value - 1) / cols;
+      int goalCol = (value - 1) % cols;
+
+      dist += fabs(row - goalRow) + fabs(col - goalCol);
+    }
+  }
+  return dist;
+}
+
+int heuristicValue(const vector<int>& board, const vector<int>& goal, int cols, const string& heuristic) {
+  // if (heuristic == "hamm") {
+  //   return hamming(board, goal);
+  // }
+
+  if (heuristic == "manh") {
+    return manhattan(board, cols);
+  }
+
+  return 0;
+}
+
+
+bool CompareAStar::operator()(const AStarNode& a, const AStarNode& b) const {
+  if (a.f == b.f) {
+    return a.h > b.h;
+  }
+  return a.f > b.f;
+}
+
+static bool DFSrec(vector<int>& Board, int rows, int cols,
+              string& path, int zeroPos=0, int depth=0,
               int maxDepth, const vector<int>& goal, Result& result,
               char lastMove, const string& order){
 
-    result.maxDepth = max(result.maxDepth, depth);;
+    result.maxDepth = max(result.maxDepth, depth);
     result.processed++;
 
-    auto startTime = chrono::high_resolution_clock::now();
-
-  if (allVisitedDFS.find(board) == allVisitedDFS.end()) {
-    allVisitedDFS.insert(board);
+  if (allVisitedDFS.find(Board) == allVisitedDFS.end()) {
+    allVisitedDFS.insert(Board);
     result.visited++;
   }
 
     if (Board == goal){
       return true;
     }
-    if (depth >= limit){
+    if (depth >= maxDepth){
       return false;
     }
 
-    pathVisitedDFS.insert(board);
+    pathVisitedDFS.insert(Board);
 
     for (char move : order) {
-      if (pathVisitedDFS.find(Board) == pathVisitedDFS.end()) {
-        if (move == 'L' && (node.zeroPos % cols) != 0 && lastMove != 'R') {
-          swap(Board[node.zeroPos], Board[node.zeroPos-1]);
-          if (DFSrec(Board, rows, cols, path, node.zeroPos, depth, maxDepth, goal, result, move)){
-             return true;
-          }
-          path.pop_back();
+
+        if (move == 'L' && ((zeroPos % cols) != 0) && (lastMove != 'R')) {
+          swap(Board[zeroPos], Board[zeroPos-1]);
+          if (pathVisitedDFS.find(Board) == pathVisitedDFS.end()){
+            path+=move;
+            if (DFSrec(Board, rows, cols, path, zeroPos - 1, depth + 1, maxDepth, goal, result, move)){
+              return true;
+            }
+            path.pop_back();
         }
-      swap(board[zeroPos], board[zeroPos - 1]);
+      swap(Board[zeroPos], Board[zeroPos - 1]);
       }
 
-      if (pathVisitedDFS.find(Board) == pathVisitedDFS.end()) {
-        if (move == 'R' && (node.zeroPos % cols) != cols && lastMove != 'L' ) {
-          swap(Board[node.zeroPos], Board[node.zeroPos+1]);
-          if (DFSrec(Board, rows, cols, path, node.zeroPos, depth, maxDepth, goal, result, move)){
+        if (move == 'R' && ((zeroPos % cols) != cols - 1) && (lastMove != 'L') ) {
+          swap(Board[zeroPos], Board[zeroPos+1]);
+          if (pathVisitedDFS.find(Board) == pathVisitedDFS.end()){
+            path+=move;
+          if (DFSrec(Board, rows, cols, path, zeroPos + 1, depth + 1, maxDepth, goal, result, move)){
             return true;
           }
           path.pop_back();
         }
-        swap(board[zeroPos], board[zeroPos + 1]);
+        swap(Board[zeroPos], Board[zeroPos + 1]);
       }
 
-      if (pathVisitedDFS.find(Board) == pathVisitedDFS.end()) {
-        if (move == 'U' && node.zeroPos > (cols-1) && lastMove != 'D' ) {
-          swap(Board[node.zeroPos], Board[node.zeroPos-cols]);
-          if (DFSrec(Board, rows, cols, path, node.zeroPos, depth, maxDepth, goal, result, move)){
+        if (move == 'U' && (zeroPos > (cols-1)) && (lastMove != 'D') ) {
+          swap(Board[zeroPos], Board[zeroPos-cols]);
+          if (pathVisitedDFS.find(Board) == pathVisitedDFS.end()){
+            path+=move;
+          if (DFSrec(Board, rows, cols, path, zeroPos - cols, depth + 1, maxDepth, goal, result, move)){
             return true;
           }
           path.pop_back();
         }
-        swap(board[zeroPos], board[zeroPos - cols]);
+        swap(Board[zeroPos], Board[zeroPos - cols]);
       }
 
-      if (pathVisitedDFS.find(Board) == pathVisitedDFS.end()) {
-        if (move == 'D' && zeroPos < (Board.size()-cols) && lastMove != 'U' ) {
-          swap(Board[node.zeroPos], Board[node.zeroPos-1]);
-          if (DFSrec(Board, rows, cols, path, node.zeroPos, depth, maxDepth, goal, result, move)){
+        if (move == 'D' && (zeroPos < (Board.size()-cols)) && (lastMove != 'U') ) {
+          swap(Board[zeroPos], Board[zeroPos + cols]);
+          if (pathVisitedDFS.find(Board) == pathVisitedDFS.end()){
+            path+=move;
+          if (DFSrec(Board, rows, cols, path, zeroPos + cols, depth + 1, maxDepth, goal, result, move)){
             return true;
           }
           path.pop_back();
         }
-        swap(board[zeroPos], board[zeroPos - 1]);
+        swap(Board[zeroPos], Board[zeroPos + cols]);
       }
     }
 
-  pathVisitedDFS.erase(board);
+  pathVisitedDFS.erase(Board);
   return false;
 }
+
 
 Result DFS(const vector<int>& startBoard, int rows, int cols, const string& order, int limit) {
   auto startTime = chrono::high_resolution_clock::now();
@@ -120,7 +169,135 @@ Result DFS(const vector<int>& startBoard, int rows, int cols, const string& orde
   }
 
   auto endTime = chrono::high_resolution_clock::now();
-  result.timeMs = chrono::duration<double, std::milli>(endTime - startTime).count();
+  result.timeMs = chrono::duration<double, milli>(endTime - startTime).count();
 
   return result;
 }
+
+Result aStar(const vector<int>& startBoard, int rows, int cols, const string& heuristic){
+  auto startTime = chrono::high_resolution_clock::now();
+
+  Result result;
+  vector<int> goal = makeGoalState(rows, cols);
+  string order = "LRUD";
+  map<vector<int>, int> bestG;
+  priority_queue<AStarNode, vector<AStarNode>, CompareAStar> pq;
+
+  AStarNode star;
+  star.state = startBoard;
+  star.path = "";
+  star.zeroPos = findZero(star.state);
+  star.lastMove = 'N';
+  star.g=0;
+  star.h=heuristicValue(startBoard, goal, cols, heuristic);
+  star.f=star.g + star.h;
+
+  pq.push(star);
+  bestG[star.state] = 0;
+  result.visited = 1;
+
+  while (!pq.empty()) {
+    AStarNode cur = pq.top();
+    pq.pop();
+
+    if (bestG.find(cur.state) != bestG.end() && cur.g > bestG[cur.state]) {
+      continue;
+    }
+
+    result.processed++;
+    result.maxDepth = max(result.maxDepth, (int)cur.path.size());
+
+    if (cur.state == goal) {
+      result.path = cur.path;
+
+      auto endTime = chrono::high_resolution_clock::now();
+      result.timeMs = chrono::duration<double, milli>(endTime - startTime).count();
+      return result;
+    }
+
+    for (char move : order) {
+      if (move == 'L' && ((cur.zeroPos % cols) != 0)){
+        AStarNode next = cur;
+        swap(next.state[cur.zeroPos], next.state[cur.zeroPos - 1]);
+        next.zeroPos = findZero(next.state);
+        next.path += move;
+        next.lastMove = move;
+        next.g++;
+        next.h = heuristicValue(next.state, goal, cols, heuristic);
+        next.f = next.g + next.h;
+
+        if (bestG.find(next.state) == bestG.end() || next.g < bestG[next.state]) {
+          if (bestG.find(next.state) == bestG.end()) {
+            result.visited++;
+          }
+         bestG[next.state] = next.g;
+          pq.push(next);
+        }
+        }
+
+        if (move == 'R' && ((cur.zeroPos % cols) != cols - 1)){
+          AStarNode next = cur;
+          swap(next.state[cur.zeroPos], next.state[cur.zeroPos + 1]);
+          next.zeroPos = findZero(next.state);
+          next.path += move;
+          next.lastMove = move;
+          next.g++;
+          next.h = heuristicValue(next.state, goal, cols, heuristic);
+          next.f = next.g + next.h;
+
+          if (bestG.find(next.state) == bestG.end() || next.g < bestG[next.state]) {
+            if (bestG.find(next.state) == bestG.end()) {
+              result.visited++;
+            }
+            bestG[next.state] = next.g;
+            pq.push(next);
+          }
+          }
+
+          if (move == 'U' && (cur.zeroPos > (cols-1))){
+             AStarNode next = cur;
+            swap(next.state[cur.zeroPos], next.state[cur.zeroPos - cols]);
+             next.zeroPos = findZero(next.state);
+             next.path += move;
+             next.lastMove = move;
+             next.g++;
+             next.h = heuristicValue(next.state, goal, cols, heuristic);
+             next.f = next.g + next.h;
+
+             if (bestG.find(next.state) == bestG.end() || next.g < bestG[next.state]) {
+               if (bestG.find(next.state) == bestG.end()) {
+                 result.visited++;
+               }
+              bestG[next.state] = next.g;
+               pq.push(next);
+             }
+            }
+            if (move == 'D' && cur.zeroPos < (cur.state.size()-cols)){
+               AStarNode next = cur;
+               swap(next.state[cur.zeroPos], next.state[cur.zeroPos + cols]);
+               next.zeroPos = findZero(next.state);
+               next.path += move;
+               next.lastMove = move;
+               next.g++;
+               next.h = heuristicValue(next.state, goal, cols, heuristic);
+               next.f = next.g + next.h;
+
+               if (bestG.find(next.state) == bestG.end() || next.g < bestG[next.state]) {
+                 if (bestG.find(next.state) == bestG.end()) {
+                   result.visited++;
+                 }
+                 bestG[next.state] = next.g;
+                 pq.push(next);
+           }
+         }
+       }
+    }
+        result.path = "NOTHING";
+
+        auto endTime = chrono::high_resolution_clock::now();
+        result.timeMs = chrono::duration<double, milli>(endTime - startTime).count();
+
+        return result;
+  }
+
+
--- src/algorithms.h
+++ src/algorithms.h
@@ -14,18 +14,51 @@ struct Node {
     char lastMove;
 };
 
+static vector<string> getSearchOrders() {
+    return {
+        "RDUL",
+        "RDLU",
+        "DRUL",
+        "DRLU",
+        "LUDR",
+        "LURD",
+        "ULDR",
+        "ULRD"
+    };
+}
+
+static vector<string> getHeuristics() {
+    return {
+        "hamm",
+        "manh"
+    };
+}
+
+struct AStarNode {
+    vector<int> state;
+    string path;
+    int zeroPos;
+    char lastMove;
+    int g;
+    int h;
+    int f;
+};
+
+struct CompareAStar {
+    bool operator()(const AStarNode& a, const AStarNode& b) const;
+};
+
 class algorithms {
     int findZero(const vector<int>& startBoard);
     vector<int> makeGoalState(int rows, int cols);
-    getSearchOrders();
-    getSearchHeuristics();
+    int heuristicValue(const vector<int>& board, const vector<int>& goal, int cols, const string& heuristic, const vector<int>& goal);
+    int manhattam (const vector<int>& board,  int rows);
     Result BFS(const vector<int>& startBoard, int rows, int cols, const string& order);
-    static bool DFSrec(const vector<int>& startBoard, int rows, int cols,
+    static bool DFSrec(vector<int>& startBoard, int rows, int cols,
                   const string& path,int zeroPos, int depth, int maxDepth,
                   const vector<int>& goal, Result& result, char move, const string& order);
-
     Result DFS(const vector<int>& startBoard, int rows, int cols, const string& order, int limit);
-    Result aStar(const vector<int>& startBoard, int rows, int cols, bool heuristic);
+    Result aStar(const vector<int>& startBoard, int rows, int cols, const string& heuristic);
 };
 
 
